e5bf198742ef8b1348ebdba1c3c7e49c8e254b7b
Geovanne-Meloni <geovanne.meloni1@gmail.com>
2025-12-02 15:02:33 -0300

    fix: update start script in package.json to include env-file and add localhost to connect-src in Content Security Policy

--- src/main.ts
+++ src/main.ts
@@ -23,7 +23,6 @@ app.use(
                 "script-src": ["'self'", "'unsafe-inline'"],
                 "style-src": ["'self'", "'unsafe-inline'"],
                 "img-src": ["'self'", "data:", "res.cloudinary.com", "validator.swagger.io"],
-                "connect-src": ["'self'", "https://navy-backend.onrender.com"],
                 "connect-src": ["'self'", "https://navy-backend.onrender.com", "http://localhost:8080", "https://navy-seminovos.onrender.com"],
             },
         },
